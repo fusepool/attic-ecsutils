13db237772d6fee4c72057f765e0a2cd5229b40e
Luigi Selmi <selmi_luigi@hotmail.com>
2013-07-31 10:37:01 +0200

    updated service

--- src/main/java/eu/fusepool/ecs/utils/EcsUtils.java
+++ src/main/java/eu/fusepool/ecs/utils/EcsUtils.java
@@ -15,16 +15,25 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 
+import org.apache.clerezza.jaxrs.utils.TrailingSlash;
 import org.apache.clerezza.platform.graphprovider.content.ContentGraphProvider;
+import org.apache.clerezza.rdf.core.MGraph;
 import org.apache.clerezza.rdf.core.TripleCollection;
+import org.apache.clerezza.rdf.core.UriRef;
 import org.apache.clerezza.rdf.core.access.LockableMGraph;
+import org.apache.clerezza.rdf.core.impl.PlainLiteralImpl;
 import org.apache.clerezza.rdf.core.serializedform.Parser;
+import org.apache.clerezza.rdf.ontologies.RDF;
+import org.apache.clerezza.rdf.ontologies.RDFS;
+import org.apache.clerezza.rdf.utils.GraphNode;
 import org.apache.felix.scr.annotations.Activate;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Deactivate;
 import org.apache.felix.scr.annotations.Property;
 import org.apache.felix.scr.annotations.Reference;
 import org.apache.felix.scr.annotations.Service;
+import org.apache.stanbol.commons.indexedgraph.IndexedMGraph;
+import org.apache.stanbol.commons.web.viewable.RdfViewable;
 import org.apache.stanbol.enhancer.servicesapi.ChainManager;
 import org.apache.stanbol.enhancer.servicesapi.ContentItemFactory;
 import org.apache.stanbol.enhancer.servicesapi.EnhancementJobManager;
@@ -41,7 +50,7 @@ import org.slf4j.LoggerFactory;
 @Component
 @Service(Object.class)
 @Property(name = "javax.ws.rs", boolValue = true)
-@Path("EcsUtils")
+@Path("ecsutils")
 public class EcsUtils {
 	
 	/**
@@ -84,6 +93,33 @@ public class EcsUtils {
     }
     
     /**
+     * This method return an RdfViewable, this is an RDF serviceUri with associated
+     * presentational information.
+     */
+    @GET
+    public RdfViewable serviceEntry(@Context final UriInfo uriInfo, 
+            @HeaderParam("user-agent") String userAgent) throws Exception {
+        //this maks sure we are nt invoked with a trailing slash which would affect
+        //relative resolution of links (e.g. css)
+        TrailingSlash.enforcePresent(uriInfo);
+        final String resourcePath = uriInfo.getAbsolutePath().toString();
+        //The URI at which this service was accessed accessed, this will be the 
+        //central serviceUri in the response
+        final UriRef serviceUri = new UriRef(resourcePath);
+        //the in memory graph to which the triples for the response are added
+        final MGraph responseGraph = new IndexedMGraph();
+        //This GraphNode represents the service within our result graph
+        final GraphNode node = new GraphNode(serviceUri, responseGraph);
+        //The triples will be added to the first graph of the union
+        //i.e. to the in-memory responseGraph
+        node.addProperty(RDF.type, Ontology.MultiEnhancer);
+        node.addProperty(RDFS.comment, new PlainLiteralImpl("A set of utilities for the ECS"));
+        //What we return is the GraphNode we created with a template path
+        return new RdfViewable("EcsUtils", node, EcsUtils.class);
+        //return "OK";
+    }
+    
+    /**
      * Test service component
      *
      */
@@ -91,11 +127,17 @@ public class EcsUtils {
     @Path("test")
     @Produces("text/plain")
     public String testService(@Context final UriInfo uriInfo,
-            @QueryParam("uri") final URL uri) throws Exception {
+            @QueryParam("query") final String query) throws Exception {
         AccessController.checkPermission(new AllPermission());
-        String userInfo = uriInfo.getBaseUri().getUserInfo();
+        String uriInfoStr = uriInfo.getRequestUri().toString();
         
-        return "Test EcsUtils service Ok. uri parameter: " + uri.toString();
+        String response = "no query. Add a query param for a response message /test?query=<your query>";
+        
+        if(!(query == null)) {
+        	response = "parrot's response: " + query;
+        }
+        
+        return "Test EcsUtils service Ok. Request URI: " + uriInfoStr + ". Response: " + response;
     }
     
     /**
@@ -136,11 +178,26 @@ public class EcsUtils {
                 + " triples";
     }
     
+    /**
+     * Select a (random) target uri from a set of equivalent uris and replace those aliases with it in all the triples in the content graph
+     *  in which these aliases are subjects or objects.
+     * @param sameAsTriples
+     * @return
+     */
     @POST
     @Path("smush")
+    @Produces("text/plain")
     public String sameAsSmush(TripleCollection sameAsTriples) {
-        SameAsSmusher.smush(getContentGraph(), sameAsTriples);
-        return "fine";
+    	String responseMessage = "";
+    	if(sameAsTriples == null || sameAsTriples.isEmpty()) {
+    		responseMessage = "Error! You must upload an RDF file containing owl:sameAs statements.";
+    		
+    	}
+    	else {
+    		SameAsSmusher.smush(getContentGraph(), sameAsTriples);
+    		responseMessage = "Smushing completed";
+    	}
+        return responseMessage;
     }
     
     /**
